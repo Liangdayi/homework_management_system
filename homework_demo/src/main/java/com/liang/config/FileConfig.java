9a2df1b6b88e38388ce6cc83b68154af559032bd
Liangdayi <50923023+Liangdayi@users.noreply.github.com>
2020-02-24 21:47:00 +0800

    修改了文件上传限制。原大小为1Mb更改为限制文件大小10MB

---
+++ homework_demo/src/main/java/com/liang/config/FileConfig.java
@@ -0,0 +1,21 @@
+package com.liang.config;
+
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+
+import javax.servlet.MultipartConfigElement;
+
+@Configuration
+public class FileConfig {
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+
+        //factory.setMaxFileSize(1024);
+        //单个文件最大
+        factory.setMaxFileSize(DataSize.parse("10240KB")); //KB,MB
+        return factory.createMultipartConfig();
+    }
+}
